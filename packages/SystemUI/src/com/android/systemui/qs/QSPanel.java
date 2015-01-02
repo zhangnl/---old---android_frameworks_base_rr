c0a656d12e52ecfb20772c4b8a461661f3e504a6
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-01-02 00:16:07 +0000

    Extend qs haptic feedback option to Status Bar Header
    also add it to detailed view toggle and buttons
    
    Change-Id: Ib5d73f406140d3276297910606f2ecac26a9c3ee

--- packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
@@ -124,6 +124,7 @@ public class QSPanel extends ViewGroup {
                 announceForAccessibility(
                         mContext.getString(R.string.accessibility_desc_quick_settings));
                 closeDetail();
+                vibrateTile(20);
             }
         });
     }
@@ -333,6 +334,7 @@ public class QSPanel extends ViewGroup {
             public void onToggleStateChanged(boolean state) {
                 if (mDetailRecord == r) {
                     fireToggleStateChanged(state);
+                    vibrateTile(20);
                 }
             }
             @Override
@@ -439,6 +441,7 @@ public class QSPanel extends ViewGroup {
                 @Override
                 public void onClick(View v) {
                     mHost.startActivityDismissingKeyguard(settingsIntent);
+                    vibrateTile(20);
                 }
             });
 
