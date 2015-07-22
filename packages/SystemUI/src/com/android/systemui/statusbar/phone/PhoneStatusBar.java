17cd596e20e15276a1274e903734b5dbdbfea81d
LorDClockaN <davor@losinj.com>
2015-07-22 06:17:38 +0000

    RR logo in the statusbar [1/2]
    Change-Id: Ie5c6026b4ddb5d361d0f07e2187e892f8fba8cab
    Signed-off-by: Varun <date.varun123@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -373,6 +373,10 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
     boolean mExpandedVisible;
 
+    // RR logo
+    private boolean mRRlogo;
+    private ImageView rrLogo;
+
     private int mNavigationBarWindowState = WINDOW_STATE_SHOWING;
 
     private int mStatusBarHeaderHeight;
@@ -463,6 +467,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.APP_SIDEBAR_POSITION),
                     false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.STATUS_BAR_RR_LOGO),
+                    false, this, UserHandle.USER_ALL);
             update();
         }
 
@@ -484,7 +491,12 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             mBrightnessControl = CMSettings.System.getIntForUser(
                     resolver, CMSettings.System.STATUS_BAR_BRIGHTNESS_CONTROL, 0,
                     UserHandle.USER_CURRENT) == 1;
-
+            mRRlogo = Settings.System.getIntForUser(resolver,
+                    Settings.System.STATUS_BAR_RR_LOGO, 0, mCurrentUserId) == 1;
+            showRRLogo(mRRlogo);
+            if (oldWeatherState != mWeatherTempState) {
+                updateTempView();
+		}
             if (mNavigationBarView != null) {
                 boolean navLeftInLandscape = CMSettings.System.getIntForUser(resolver,
                         CMSettings.System.NAVBAR_LEFT_IN_LANDSCAPE, 0, UserHandle.USER_CURRENT) == 1;
@@ -549,6 +561,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         mNavigationBarView.setBar(this);
         addNavigationBar();
     }
+
+
+
 
     // ensure quick settings is disabled until the current user makes it through the setup wizard
     private boolean mUserSetup = false;
@@ -3495,6 +3510,15 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             }
         }
     };
+
+    public void showRRLogo(boolean show) {
+        if (mStatusBarView == null) return;
+        ContentResolver resolver = mContext.getContentResolver();
+        rrLogo = (ImageView) mStatusBarView.findViewById(R.id.rr_logo);
+        if (rrLogo != null) {
+            rrLogo.setVisibility(show ? (mRRlogo ? View.VISIBLE : View.GONE) : View.GONE);
+        }
+    }
 
     private void resetUserExpandedStates() {
         ArrayList<Entry> activeNotifications = mNotificationData.getActiveNotifications();
--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarWindowView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarWindowView.java
@@ -258,6 +258,20 @@ public class StatusBarWindowView extends FrameLayout {
         if (mDoubleTapToSleepEnabled
                 && ev.getY() < mStatusBarHeaderHeight) {
             if (DEBUG) Log.w(TAG, "logging double tap gesture");
+		}
+        final int h = getMeasuredHeight();
+        if (mDoubleTapToSleepEnabled) {
+            if (mService.getBarState() == StatusBarState.SHADE
+                    && ev.getY() < mStatusBarHeaderHeight) {
+                if (DEBUG) Log.w(TAG, "logging double tap gesture");
+                mDoubleTapGesture.onTouchEvent(ev);
+            } else if (mService.getBarState() == StatusBarState.KEYGUARD
+                    && (ev.getY() < (h / 3) ||
+                    ev.getY() > (h - mStatusBarHeaderHeight))) {
+                if (DEBUG) Log.w(TAG, "logging lock screen double tap gesture");
+                mDoubleTapGesture.onTouchEvent(ev);
+           		 }
+        	}
             mDoubleTapGesture.onTouchEvent(ev);
         }
         if (mNotificationPanel.isFullyExpanded()
