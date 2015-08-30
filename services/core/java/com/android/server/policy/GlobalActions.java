24ae0584ff2a4261f6a700f008d0988ac8e9ab15
Alex Cruz <mazdarider23@gmail.com>
2015-08-30 12:19:44 -0400

    Lockscreen colors [1/2]
    Inspired by what SlimROMs did back in kitkat, I decided to make the lockscreen
    elements themeable via the color changer
    
    - Lockscreen bottom shortcuts
    - Lockscreen lock icon
    - Lockscreen clock & date
    - Lockscreen indication text
    
    Signed-off-by: Varun <date.varun123@gmail.com>

--- services/core/java/com/android/server/policy/GlobalActions.java
+++ services/core/java/com/android/server/policy/GlobalActions.java
@@ -852,7 +852,8 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
         startIntent.setComponent(cn);
         startIntent.setAction("start");
         mContext.startService(startIntent);
-}
+
+	}
     
 
     private void prepareDialog() {
