c38d35b402f49f652341e4f1ca75d680d532fac2
Varun <date.varun123@gmail.com>
2015-11-25 20:53:17 +0530

    Revert "SystemUI: extend qs haptic feedback option to Status Bar Header"
    This reverts commit ead461996d8b5ccae7294d2c150f2b9d68d907d9.

--- packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
@@ -128,7 +128,6 @@ public class QSPanel extends ViewGroup {
                 announceForAccessibility(
                         mContext.getString(R.string.accessibility_desc_quick_settings));
                 closeDetail();
-                vibrateTile(20);
             }
         });
     }
@@ -339,7 +338,6 @@ public class QSPanel extends ViewGroup {
             public void onToggleStateChanged(boolean state) {
                 if (mDetailRecord == r) {
                     fireToggleStateChanged(state);
-                    vibrateTile(20);
                 }
             }
             @Override
@@ -446,7 +444,6 @@ public class QSPanel extends ViewGroup {
                 @Override
                 public void onClick(View v) {
                     mHost.startActivityDismissingKeyguard(settingsIntent);
-                    vibrateTile(20);
                 }
             });
 
