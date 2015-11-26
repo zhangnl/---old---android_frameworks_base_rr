203abed5d7f881caac6e7aef37dd96273f2d8858
Varun <date.varun123@gmail.com>
2015-11-26 06:57:02 +0530

    Revert "Add Haptic Feedback to QS tiles [1/2]"
    This reverts commit 00bbc4990a65efbea1ae1cd117d70a06af449c82.

--- packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
@@ -26,7 +26,6 @@ import android.content.res.Resources;
 import android.os.Handler;
 import android.os.Message;
 import android.os.UserHandle;
-import android.os.Vibrator;
 import android.provider.Settings;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
@@ -84,7 +83,8 @@ public class QSPanel extends ViewGroup {
     protected QSFooter mFooter;
     private boolean mGridContentVisible = true;
 
-    protected Vibrator mVibrator;
+
+
 
     public QSPanel(Context context) {
         this(context, null);
@@ -111,7 +111,6 @@ public class QSPanel extends ViewGroup {
         addView(mBrightnessView);
         addView(mFooter.getView());
         mClipper = new QSDetailClipper(mDetail);
-        mVibrator = (Vibrator) mContext.getSystemService(Context.VIBRATOR_SERVICE);
         updateResources();
 
         mBrightnessController = new BrightnessController(getContext(),
@@ -145,19 +144,6 @@ public class QSPanel extends ViewGroup {
         }
         updateResources();
         return brightnessSliderEnabled;
-    }
-
-
-    public boolean isVibrationEnabled() {
-        return (Settings.System.getIntForUser(mContext.getContentResolver(),
-                Settings.System.QUICK_SETTINGS_TILES_VIBRATE, 0, UserHandle.USER_CURRENT) == 1);
-    }
-
-    public void vibrateTile(int duration) {
-        if (!isVibrationEnabled()) { return; }
-        if (mVibrator != null) {
-            if (mVibrator.hasVibrator()) { mVibrator.vibrate(duration); }
-        }
     }
 
     protected void updateDetailText() {
@@ -353,21 +339,18 @@ public class QSPanel extends ViewGroup {
             @Override
             public void onClick(View v) {
                 r.tile.click();
-                vibrateTile(20);
             }
         };
         final View.OnClickListener clickSecondary = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 r.tile.secondaryClick();
-                vibrateTile(20);
             }
         };
         final View.OnLongClickListener longClick = new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View v) {
                 r.tile.longClick();
-                vibrateTile(20);
                 return true;
             }
         };
