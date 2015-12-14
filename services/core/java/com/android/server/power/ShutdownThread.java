mintkat/frameworks_base

Add Restart SystemUI to reboot menu
Change-Id: Ie00ffd52fec29e47ab8613d6feaf065542b40bd3

services/core/java/com/android/server/power/ShutdownThread.java
@@ -71,6 +71,8 @@ import java.io.OutputStreamWriter;
 
 import java.lang.reflect.Method;
 
+import com.android.internal.util.xd.Helpers;
+
 public final class ShutdownThread extends Thread {
     // constants
     private static final String TAG = "ShutdownThread";
@@ -81,6 +83,7 @@ public final class ShutdownThread extends Thread {
     private static final int MAX_RADIO_WAIT_TIME = 12*1000;
 
     private static final String SOFT_REBOOT = "soft_reboot";
+    private static final String SYSTEMUI_REBOOT = "systemui_reboot";
 
     // length of vibration before shutting down
     private static final int SHUTDOWN_VIBRATE_MS = 500;
@@ -260,7 +263,11 @@ public final class ShutdownThread extends Thread {
             if (selected != ListView.INVALID_POSITION) {
                 String actions[] = context.getResources().getStringArray(
                         com.android.internal.R.array.shutdown_reboot_actions);
-                if (selected >= 0 && selected < actions.length) {
+                                    if (actions[selected].equals(SYSTEMUI_REBOOT)) {
+                                        mRebootReason = actions[selected];
+                                        doSystemUIReboot();
+                                        return;
+                                    } else if (selected >= 0 && selected < actions.length) {
                     mRebootReason = actions[selected];
                     if (actions[selected].equals(SOFT_REBOOT)) {
                         doSoftReboot();
@@ -284,6 +291,10 @@ public final class ShutdownThread extends Thread {
         } catch (RemoteException e) {
             Log.e(TAG, "failure trying to perform soft reboot", e);
         }
+    }
+
+    private static void doSystemUIReboot() {
+        Helpers.restartSystemUI();
     }
 
     private static class CloseDialogReceiver extends BroadcastReceiver

zhangnl/---old---android_frameworks_base_rr

services/core/java/com/android/server/power/ShutdownThread.java
@@ -78,6 +78,8 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.OutputStreamWriter;
 
+import com.android.internal.util.benzo.Helpers;
+
 public final class ShutdownThread extends Thread {
     // constants
     private static final String TAG = "ShutdownThread";
@@ -95,6 +97,7 @@ public final class ShutdownThread extends Thread {
     private static final int MOUNT_SERVICE_STOP_PERCENT = 20;
 
     private static final String SOFT_REBOOT = "soft_reboot";
+    private static final String SYSTEMUI_REBOOT = "systemui_reboot";
 
     // length of vibration before shutting down
     private static final int SHUTDOWN_VIBRATE_MS = 500;
@@ -255,7 +258,11 @@ public final class ShutdownThread extends Thread {
                                 if (selected != ListView.INVALID_POSITION) {
                                     String actions[] = context.getResources().getStringArray(
                                             com.android.internal.R.array.shutdown_reboot_actions);
-                                    if (selected >= 0 && selected < actions.length) {
+                                    if (actions[selected].equals(SYSTEMUI_REBOOT)) {
+                                        mRebootReason = actions[selected];
+                                        doSystemUIReboot();
+                                        return;
+                                    } else if (selected >= 0 && selected < actions.length) {
                                         mRebootReason = actions[selected];
                                         if (actions[selected].equals(SOFT_REBOOT)) {
                                             doSoftReboot();
@@ -292,6 +299,10 @@ public final class ShutdownThread extends Thread {
         } catch (RemoteException e) {
             Log.e(TAG, "failure trying to perform soft reboot", e);
         }
+    }
+
+    private static void doSystemUIReboot() {
+        Helpers.restartSystemUI();
     }
 
     private static class CloseDialogReceiver extends BroadcastReceiver