e28e2a28230ef2025d432c7f9af3753803766da1
westcripp <korukaltan@gmail.com>
2016-03-13 03:58:17 +0200

    Fix lockscreen clock and date not updating

--- packages/Keyguard/src/com/android/keyguard/KeyguardStatusView.java
+++ packages/Keyguard/src/com/android/keyguard/KeyguardStatusView.java
@@ -91,9 +91,9 @@ public class KeyguardStatusView extends GridLayout implements
 
         @Override
         public void onTimeChanged() {
-			updateClockColor();
-            updateClockDateColor();
             if (mEnableRefresh) {
+                updateClockColor();
+                updateClockDateColor();
                 refresh();
             }
         }
@@ -102,25 +102,25 @@ public class KeyguardStatusView extends GridLayout implements
         public void onKeyguardVisibilityChanged(boolean showing) {
             if (showing) {
                 if (DEBUG) Slog.v(TAG, "refresh statusview showing:" + showing);
-                refresh();
-                updateOwnerInfo();
                 updateClockColor();
                 updateClockDateColor();
+                refresh();
+                updateOwnerInfo();
             }
         }
 
         @Override
         public void onStartedWakingUp() {
             setEnableMarquee(true);
-			updateClockColor();
-            updateClockDateColor();
             mEnableRefresh = true;
+            updateClockColor();
+            updateClockDateColor();
             refresh();
         }
 
         @Override
         public void onFinishedGoingToSleep(int why) {
-			updateClockColor();
+            updateClockColor();
             updateClockDateColor();
             setEnableMarquee(false);
             mEnableRefresh = false;
@@ -130,8 +130,6 @@ public class KeyguardStatusView extends GridLayout implements
         public void onUserSwitchComplete(int userId) {
             refresh();
             updateOwnerInfo();
-            updateClockColor();
-            updateClockDateColor();
         }
     };
 
@@ -343,6 +341,10 @@ public class KeyguardStatusView extends GridLayout implements
                 Settings.System.HIDE_LOCKSCREEN_CLOCK, 1, UserHandle.USER_CURRENT) == 1;
         boolean showDate = Settings.System.getIntForUser(resolver,
                 Settings.System.HIDE_LOCKSCREEN_DATE, 1, UserHandle.USER_CURRENT) == 1;
+        int clockColor = Settings.System.getInt(resolver,
+                Settings.System.LOCKSCREEN_CLOCK_COLOR, 0xFFFFFFFF);
+        int clockDateColor = Settings.System.getInt(resolver,
+                Settings.System.LOCKSCREEN_CLOCK_DATE_COLOR, 0xFFFFFFFF);
         int mTempColor = Settings.System.getInt(resolver,
                 Settings.System.LOCK_SCREEN_WEATHER_TEMP_COLOR, 0xFFFFFFFF);
        	int mConditionColor = Settings.System.getInt(resolver,
@@ -351,7 +353,7 @@ public class KeyguardStatusView extends GridLayout implements
                 Settings.System.LOCK_SCREEN_WEATHER_STAMP_COLOR, 0xFFFFFFFF);
         int mHumidityColor = Settings.System.getInt(resolver,
                 Settings.System.LOCK_SCREEN_WEATHER_HUM_COLOR, 0xFFFFFFFF);
-	int mCityColor = Settings.System.getInt(resolver,
+        int mCityColor = Settings.System.getInt(resolver,
                 Settings.System.LOCK_SCREEN_WEATHER_CITY_COLOR, 0xFFFFFFFF);
         int mWindColor = Settings.System.getInt(resolver,
                 Settings.System.LOCK_SCREEN_WEATHER_WIND_COLOR, 0xFFFFFFFF);
@@ -515,6 +517,14 @@ public class KeyguardStatusView extends GridLayout implements
         }
         if (lockClockFont == 24) {
             mClockView.setTypeface(Typeface.create("serif", Typeface.BOLD_ITALIC));
+        }
+
+        if (mClockView != null) {
+              mClockView.setTextColor(clockColor);
+        }
+
+        if (mDateView != null) {
+            mDateView.setTextColor(clockDateColor);
         }
 
         if (mOwnerInfo != null) {
