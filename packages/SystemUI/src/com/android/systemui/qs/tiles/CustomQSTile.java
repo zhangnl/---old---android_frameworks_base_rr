5fdb7c68d41d4978dcd7f8f942f1ea3c0f3f0633
Varun Date <date.varun123@gmail.com>
2016-03-28 09:50:45 +0530

    Revert "SystemUi: Respect shouldCollapsePanel when handling onClick."
    This reverts commit daafc613c7f6e27ee8cd95127c945be619b99ebd.

--- packages/SystemUI/src/com/android/systemui/qs/tiles/CustomQSTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/CustomQSTile.java
@@ -59,7 +59,6 @@ public class CustomQSTile extends QSTile<QSTile.State> {
     private int mCurrentUserId;
     private StatusBarPanelCustomTile mTile;
     private CustomQSDetailAdapter mDetailAdapter;
-    private boolean mCollapsePanel;
 
     public CustomQSTile(Host host, StatusBarPanelCustomTile tile) {
         super(host);
@@ -103,12 +102,11 @@ public class CustomQSTile extends QSTile<QSTile.State> {
             if (mExpandedStyle != null &&
                     mExpandedStyle.getStyle() != CustomTile.ExpandedStyle.NO_STYLE) {
                 showDetail(true);
-                return;
-            }
-            if (mCollapsePanel) {
-                mHost.collapsePanels();
             }
             if (mOnClick != null) {
+                if (mOnClick.isActivity()) {
+                    mHost.collapsePanels();
+                }
                 mOnClick.send();
             } else if (mOnClickUri != null) {
                 mHost.collapsePanels();
@@ -141,7 +139,6 @@ public class CustomQSTile extends QSTile<QSTile.State> {
         mOnClick = customTile.onClick;
         mOnClickUri = customTile.onClickUri;
         mExpandedStyle = customTile.expandedStyle;
-        mCollapsePanel = customTile.collapsePanel;
         mDetailAdapter = new CustomQSDetailAdapter();
     }
 
